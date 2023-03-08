f5f036a9cea98aab1f1fa91671b10d8417df3699
aaron <2019230854@unfv.edu.pe>
2023-03-08 16:44:34 -0500

    ga

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Grid } from 'gridjs-react';
+import React, { useState } from 'react';
+import { faker } from '@faker-js/faker';
+import "gridjs/dist/theme/mermaid.css";
 
 function App() {
+
+  // new gridjs.Grid({
+  //   columns: { "id", "nombre"},
+  //   data: ["1", "aaron"]
+  // }).render(document.getElementById("table"));
+
+  // const grid = new Grid({
+  //   columns: ['Name', 'Email', 'Phone Number'],
+  //   data: [
+  //     ['John', 'john@example.com', '(353) 01 222 3333'],
+  //     ['Mark', 'mark@gmail.com',   '(01) 22 888 4444']
+  //   ]
+  // }).render(document.getElementById("table"));
+
+  const row = () => [faker.name.findName(), faker.internet.email()];
+  const [data, setData] = useState([row()]);
+  const update = () => {
+    setData(data.slice(0).concat([row()]));
+  }
+
+  // return (
+  //   <div className="App">
+  //     <div id='table'>
+
+  //     </div>
+  //   </div>
+    
+  // );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={update.bind(this)} className={"py-2 mb-4 px-4 border rounded-md text-white bg-blue-600"}>
+        Add record
+      </button>
+      
+      <Grid
+        data={data}
+        columns={['Name', 'Email']}
+        pagination={{
+          limit: 5,
+        }}
+      />
     </div>
+
+    
   );
 }
 
